14658e3bf5be17cbfa5fb8af5bf6a496e1d6e6bf
ngDuyThong <thongduy0512@gmail.com>
2023-09-14 09:53:18 +0700

    first update

---
+++ components/common/MainNavBar.vue
@@ -0,0 +1,17 @@
+<template>
+    <ul>
+        <li>Home Page</li>
+        <li>About</li>
+        <li>Course</li>
+        <li>Event</li>
+        <li>News</li>
+        <li>Excellent students</li>
+        <li>Test&Exams</li>
+    </ul>
+</template>
+
+<script>
+export default {
+
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Home Page</h1>
+    <MainNavBar></MainNavBar>
+    <PartnersCarousel></PartnersCarousel>
+  </div>
+</template>
+
+<script>
+import MainNavBar from "../components/common/MainNavBar.vue"
+import PartnersCarousel from "../components/common/PartnersCarousel.vue"
+export default {
+  data() {
+    return {
+
+    }
+  },
+  components: {
+    MainNavBar,
+    PartnersCarousel
+  }
+}
+</script>
