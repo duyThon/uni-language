91bc303d9b2421b32bf1af647b5608094fc51b23
ngDuyThong <thongduy0512@gmail.com>
2023-09-17 00:38:35 +0700

    update navbar and footer

---
+++ components/common/Footer.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="footer">
+    <el-row :gutter="20">
+      <el-col class="footer-info" :xs="24" :sm="8" :lg="8">
+        <img class="homepage-logo-img" src="@/assets/logo/logo-uni-kem-slogan-doc-white.png" alt="UNI Language House">
+        <div class="social-media">
+          <div class="social-media-icon">
+            <a href="http://www.facebook.com/Anh.Ngu.Quoc.Te.Son.La" target="_blank"><img src="@/assets/icons/social-media-icons/fb-icon.png" alt="">facebook.com/Anh.Ngu.Quoc.Te.Son.La</a>
+            <a href="http://www.facebook.com/Anh.Ngu.Quoc.Te.Son.La" target="_blank"><img src="@/assets/icons/social-media-icons/gmail-icon.png" alt="">unilanguagesonla1@gmail.com</a>
+            <a href="http://www.facebook.com/Anh.Ngu.Quoc.Te.Son.La" target="_blank"><img src="@/assets/icons/social-media-icons/zalo-icon.png" alt="">Uni Customer Care</a>
+          </div>
+          <div class="tel">
+            <svg fill="#fff" class="telephone" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z"/></svg>
+            <span> +84 398 185 925</span>
+          </div>
+        </div>
+      </el-col>
+      <el-col class="footer-info" :xs="24" :sm="8" :lg="8">
+        <div class="" style="background-color:rgba(255,255,255,0.3);width:100%;height:500px">giới thiệu</div>
+        <!-- <img class="homepage-logo-img" src="@/assets/logo/logo-uni-kem-slogan-doc-white.png" alt="UNI Language House"> -->
+      </el-col>
+      <el-col class="footer-info" :xs="24" :sm="8" :lg="8">
+        <div class="" style="background-color:rgba(255,255,255,0.3); width:100%;height:500px">khóa học</div>
+        <!-- <img class="homepage-logo-img" src="@/assets/logo/logo-uni-kem-slogan-doc-white.png" alt="UNI Language House"> -->
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.footer {
+  padding: 1.25em;
+  background-color: var(--primary-color);
+}
+
+.footer-info img {
+  width:200px
+}
+
+/* social media */
+.social-media-icon {
+  display: flex;
+  flex-direction: column;
+  
+}
+
+.social-media-icon a {
+  text-decoration: none;
+  color: #fff;
+  display: flex;
+  align-items: center;
+  font-size: 12px;
+}
+
+.social-media img{
+  width: 1.875em;
+  margin: 6px
+}
+
+.social-media-icon a:hover {
+  opacity: .5;
+}
+
+.tel {
+  border: 3px solid #fff;
+  width: 192px;
+  border-radius: 18px;
+  height: 40px;
+  display: flex;
+  justify-content: space-evenly;
+  margin: 10px;
+}
+
+.tel span {
+  display: flex;
+  align-items: center;
+  color: #fff;
+}
+
+.tel:hover {
+  
+  animation: fadeInTel linear .2s;
+  background-color: rgba(255, 255, 255, 0.15);
+  cursor: pointer;
+}
+
+.telephone {
+  width: 24px;
+}
+
+@keyframes fadeInTel {
+    from {
+        opacity: 0.5;
+    }
+    to {
+        opacity: 1;
+    }
+}
+</style>
--- components/common/MainNavBar.vue
+++ components/common/MainNavBar.vue
@@ -1,18 +1,38 @@
 <template>
     <nav>
-        <div class="main-navbar">
-            <div class="logo">
-                <img class="homepage-logo-img" src="@/assets/logo/logo-uni-ngang.png" alt="UNI Language House">
+        <div class="main-navbar" style="padding: 1.25em;">
+            <div class="uni-logo">
+                <img class="homepage-logo-img" src="@/assets/logo/logo-uni-ngang-2.png" alt="UNI Language House">
             </div>
             <div class="main-navbar-list">
-                <ul style="margin: 0">
-                    <li class="main-navbar-list-item">About</li>
-                    <li class="main-navbar-list-item">Course</li>
-                    <li class="main-navbar-list-item">Event</li>
-                    <li class="main-navbar-list-item">News</li>
-                    <li class="main-navbar-list-item">Excellent students</li>
-                    <li class="main-navbar-list-item">Test&Exams</li>
-                </ul>
+                <el-menu style="margin: 0"
+                    mode="horizontal"
+                    @select="handleSelect"
+                    text-color="var(--text-color)">
+                    <el-menu-item index="1">GIỚI THIỆU</el-menu-item>
+                    <el-submenu index="2">
+                        <template slot="title">KHÓA HỌC</template>
+                        <el-menu-item index="2-1">Khóa giao tiếp tiêu chuẩn Cambridge</el-menu-item>
+                        <el-submenu index="2-4">
+                        <template slot="title">Khóa học chuyên sâu ngắn hạn</template>
+                        <el-menu-item index="2-4-1">Khóa học steam tiếng anh</el-menu-item>
+                        <el-menu-item index="2-4-2">Khóa học thuyết trình</el-menu-item>
+                        <el-menu-item index="2-4-3">Khóa học tranh biện</el-menu-item>
+                        </el-submenu>
+                        <el-submenu index="3-4">
+                        <template slot="title">Khóa luyện thi</template>
+                        <el-menu-item index="3-4-1">Khóa luyện thi IELT</el-menu-item>
+                        <el-menu-item index="3-4-2">Khóa luyện thi ngữ pháp</el-menu-item>
+                        <el-menu-item index="3-4-3">Khóa luyện thi chuyển cấp</el-menu-item>
+                        <el-menu-item index="3-4-4">Khóa luyện thi TOEIC</el-menu-item>
+                        <el-menu-item index="3-4-5">Khóa luyện thi Cambridge</el-menu-item>
+                        </el-submenu>
+                    </el-submenu>
+                    <el-menu-item index="3">SỰ KIỆN</el-menu-item>
+                    <el-menu-item index="3">TIN TỨC</el-menu-item>
+                    <el-menu-item index="3">HỌC VIÊN XUẤT SẮC</el-menu-item>
+                    <el-menu-item index="3">THI THỬ</el-menu-item>
+                </el-menu>
                 <div class="switch-language">
                     <ul class="nation-logo">
                         <li class="nation-logo-item">
@@ -24,60 +44,192 @@
                     </ul>
                 </div>
             </div>
-        </div>
-            <div class="main-navbar-btn">
+            <label for="nav-mobile-input" class="main-navbar-btn">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
+            </label>
+        </div>
+        <input hidden type="checkbox" id="nav-mobile-input">
+        <label for="nav-mobile-input" class="nav-overlay"></label>
+        <div class="main-navbar-list-moblie">
+            <div class="main-navbar-list-moblie-close">
+                <span>MENU</span>
+                <label for="nav-mobile-input" >
+                    <svg fill="#333" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>
+                </label>
             </div>
+            <el-menu style="margin: 30px 0 0 0"
+                mode="el-menu-vertical-demo"
+                @select="handleSelect"
+                text-color="var(--text-color)">
+                <el-menu-item index="1">GIỚI THIỆU</el-menu-item>
+                <el-submenu index="2">
+                    <template slot="title">KHÓA HỌC</template>
+                    <el-menu-item index="2-1">Khóa giao tiếp tiêu chuẩn Cambridge</el-menu-item>
+                    <el-submenu index="2-4">
+                    <template slot="title">Khóa học chuyên sâu ngắn hạn</template>
+                    <el-menu-item index="2-4-1">Khóa học steam tiếng anh</el-menu-item>
+                    <el-menu-item index="2-4-2">Khóa học thuyết trình</el-menu-item>
+                    <el-menu-item index="2-4-3">Khóa học tranh biện</el-menu-item>
+                    </el-submenu>
+                    <el-submenu index="3-4">
+                    <template slot="title">Khóa luyện thi</template>
+                    <el-menu-item index="3-4-1">Khóa luyện thi IELT</el-menu-item>
+                    <el-menu-item index="3-4-2">Khóa luyện thi ngữ pháp</el-menu-item>
+                    <el-menu-item index="3-4-3">Khóa luyện thi chuyển cấp</el-menu-item>
+                    <el-menu-item index="3-4-4">Khóa luyện thi TOEIC</el-menu-item>
+                    <el-menu-item index="3-4-5">Khóa luyện thi Cambridge</el-menu-item>
+                    </el-submenu>
+                </el-submenu>
+                <el-menu-item index="3">SỰ KIỆN</el-menu-item>
+                <el-menu-item index="3">TIN TỨC</el-menu-item>
+                <el-menu-item index="3">HỌC VIÊN XUẤT SẮC</el-menu-item>
+                <el-menu-item index="3">THI THỬ</el-menu-item>
+            </el-menu>
+            <div class="switch-language-mobile">
+                <div class="switch-language-mobile-title">NGÔN NGỮ</div>
+                <ul class="nation-logo-mobile">
+                    <li class="nation-logo-item-mobile">
+                        <img src="@/assets/icons/nation-logo/vietnam-flag-rounded-icon.webp" alt="Vietnamese">
+                    </li>
+                    <li class="nation-logo-item-mobile">
+                        <img src="@/assets/icons/nation-logo/uk-flag-rounded-icon.webp" alt="English">
+                    </li>
+                </ul>
+            </div>
+        </div>
     </nav>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+        }
+    },
 
+    methods: {
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
 }
 </script>
 
 <style scoped>
 .main-navbar {
+    z-index: 99;
+    position: fixed;
+    top: 0;
+    width: 100%;
     font-weight: 600;
-    padding: 1.25em;
+    padding: 0.25rem 1.25rem;
     display: flex;
     justify-content: space-between;
+    align-items: center;
+    background-color: rgba(255, 255, 255, .5);
+    backdrop-filter: blur(5px);
 }
 
 .homepage-logo-img {
-    height: 5em;
+    height: 4em;
 }
 
 .main-navbar-list {
     display: flex;
 }
-    
+/* list css */
 .main-navbar-list ul {
     display: flex;
     list-style: none;
+    align-items: center;
+    justify-content: center;
 }
 
 .main-navbar-list-item {
-    padding: 0.625em 2.25em 0.5em;
+    padding: 0.625em 1.25em 0.5em;
     cursor: pointer;
     display: flex;
     align-items: center;
 }
 
 .main-navbar-list-item:hover {
+    background-color: transparent !important;
     border-bottom: 0.375em solid var(--primary-color);
 }
 
+.el-menu.el-menu--horizontal {
+    border-bottom: none;
+    background-color: transparent !important;
+}
+
+.el-menu-item {
+    background-color: transparent !important;
+}
+
+.el-menu-item:hover  {
+    background-color: transparent !important;
+    border-bottom: 0.175em solid var(--primary-color) !important;
+}
+
+.el-submenu.is-opened {
+    border-bottom: 0.175em solid var(--primary-color) !important;
+}
+
+/* nation css */
+.switch-language {
+    display: flex;
+    margin-right: 34px;
+}
+
+.nation-logo {
+    padding: 0;
+}
+
 .nation-logo-item img{
-    width: 1.875em;
-    height: 1.875em;
+    width: 1.75em;
+    height: 1.75em;
     cursor: pointer;
-    margin: 0.625em;
+    margin: 0.25em;
 }
 
 .nation-logo-item img:hover {
+    border: 2px solid var(--primary-color);
+    border-radius: 50%;
+}
 
+/* overlay */
+.nav-overlay {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, .3);
+    display: none;
+    z-index: 101;
+    animation: fadeIn linear .2s;
+}
+
+.main-navbar-mobile {
+    display: none;
+}
+
+.arrow-down {
+    width: 0.65em;
+}
+
+.el-dropdown {
+    color: #333;
+    font-size: 1em;
+}
+
+.el-dropdown-menu__item{
+    font-weight: 500;
+}
+
+.el-dropdown-menu__item:hover {
+    background-color: #282e6599 !important;
+    color: #fff!important;
 }
 
 /* mobile nav */
@@ -85,35 +237,103 @@ export default {
     display: none;
 }
 
-/* tablet & mobile */
-@media (max-width: 63.9375em) {
-nav {
+.main-navbar-list-moblie {
+    position: fixed;
+    top:0;
+    right:0;
+    bottom: 0;
+    width: 320px;
+    max-width: 100%;
+    z-index: 100;
+    background-color: #fff;
+    font-weight: 500;
+    z-index: 102;
+    transform: translateX(100%);
+    opacity: 0;
+    transition: all .2s linear;
+}
+
+.main-navbar-mobile {
+    display: block;
+}
+
+.switch-language-mobile {
+    border-top: 2px solid var(--primary-color);
+}
+
+.switch-language-mobile-title {
+    margin-top: 20px;
+    padding: 0 20px
+}
+
+.nation-logo-mobile {
+    padding: 0 20px;
+    list-style-type: none;
     display: flex;
 }
 
-.main-navbar {
-    width: 100%;
-    justify-content: center;
-}
-
-.main-navbar-btn {
-    display: flex;
-    padding: 40px 40px 40px 0
-}
-
-.main-navbar-btn svg{
+.nation-logo-item-mobile img{
     width: 1.75em;
+    height: 1.75em;
+    cursor: pointer;
+    margin: 0.25em;
 }
 
-.main-navbar-list ul {
-    display: none;
+.nation-logo-item-mobile img:hover {
+    border: 2px solid var(--primary-color);
+    border-radius: 50%;
+}
+
+.main-navbar-list-moblie-close {
+    padding-bottom: 10px;
+    border-bottom:3px solid var(--primary-color) ;
+    padding: 20px;
+    font-weight: 600;
+}
+
+.main-navbar-list-moblie-close svg{
+    position: absolute;
+    top: 14px;
+    right: 10px;
+    width: 20px;
+}
+
+#nav-mobile-input:checked ~ .nav-overlay {
+    display: block;
+}
+
+#nav-mobile-input:checked ~ .main-navbar-list-moblie{
+    transform: translateX(0%);
+    opacity: 1;
 }
     
+
+/* tablet & mobile */
+@media (max-width: 63.9375em) {
+    nav {
+        display: flex;
+    }
+
+    .main-navbar {
+        width: 100%;
+    }
+
+    .main-navbar-btn {
+        display: flex;
+        padding: 40px 40px 40px 0
+    }
+
+    .main-navbar-btn svg{
+        width: 1.75em;
+    }
+
+    .main-navbar-list ul {
+        display: none;
+    }
 }
 
 /* tablet */
 @media (min-width: 46.25em) and (max-width: 63.9375em){
-    
 }
 
 </style>
